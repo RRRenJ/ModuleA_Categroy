7fa9cf93c890176d632e2d4c827e65021e0e8a79
RRRenj <rrrenj@outlook.com>
2019-06-28 11:17:52 +0800

    Initial Commit

---
+++ ModuleA_Categroy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModuleA_Categroy
+//
+//  Created by 任敬 on 2019/6/28.
+//  Copyright © 2019 任敬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModuleA_Categroy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleA_Categroy
+//
+//  Created by 任敬 on 2019/6/28.
+//  Copyright © 2019 任敬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
